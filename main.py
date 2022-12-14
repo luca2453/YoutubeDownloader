02809b66227b9e0ef244ee88d1912d7bfd3df035
Luca <luca.c2453@gmail.com>
2022-12-14 10:56:59 -0300

    Youtube Downloader mp4

---
+++ main.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+
+def Download(link):
+    
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    
+    try:
+        youtubeObject.download()
+    except:
+        print("There has been an error! Please try again!")
+    
+    print("Download complete!")
+    return
+
+link = input("Put your YouTube link here: ")
+Download(link)
